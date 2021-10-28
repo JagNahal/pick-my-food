f57a621cc0f913366c63ddcd59a5e01ddd21ab62
Jagjeevan Nahal <>
2021-10-28 02:23:02 +0000

    cost and type_of_food are no longer global variables, removed "and" from type_of_food (japanese)

--- main.py
+++ main.py
@@ -15,49 +15,53 @@ import random
 ⢸⣿⣿⣿⣿⣿⣿⣿⣿⡆⠈⠿⠿⠿⠿⠿⠿⠿⠿⠿⣧⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿
 ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿"""
 
+
 def price():
     print('\nFancy = Over $30\n' 'Normal = $15 - $30\n' 'Cheap = Under $15\n')
-    global cost
     flag = True
     while flag == True:
-      cost = input("How expensive? [F]ancy, [N]ormal [C]heap: ").upper()
-      if cost == 'F' or 'N' or 'C':
-        break
-      else:
-        print('\nInvalid Input (Choose between F, N, or C)\n ')
-        continue      
+        cost = input("How expensive? [F]ancy, [N]ormal [C]heap: ").upper()
+        if (cost == 'F') or (cost == 'N') or (cost == 'C'):
+            print(cost)
+            break
+        else:
+            print('\nInvalid Input (Choose between F, N, or C)\n ')
+            continue
+    return cost
+
 
 def cuisine():
-    global type_of_food  
     type_of_food = input("\nWhich Cuisine? ").upper()
+    return type_of_food
+
 
 cheap_american = ('McD', 'Wendy\'s', 'A&W', 'Carl\'s Jr', 'Burger King KEKW')
 
 initialize = input()
 if initialize == ".":
-  price()
-  cuisine()
+    cost = price()
+    type_of_food = cuisine()
 
 if type_of_food == 'KOREAN':
-  if cost =='C':
-    print('Daniel\'s House')
-  elif cost == 'N':
-    print('Nadri')
-  elif cost == 'F':  
-    print('Kook KBBQ')
+    if cost == 'C':
+        print('Daniel\'s House')
+    elif cost == 'N':
+        print('Nadri')
+    elif cost == 'F':
+        print('Kook KBBQ')
 
 if type_of_food == 'AMERICAN':
-  if cost =='C':
-    print(random.choice(cheap_american))
-  elif cost == 'N':
-    print('Cactus Club')
-  elif cost == 'F':  
-    print('Hy\'s Steakhouse')
+    if cost == 'C':
+        print(random.choice(cheap_american))
+    elif cost == 'N':
+        print('Cactus Club')
+    elif cost == 'F':
+        print('Hy\'s Steakhouse')
 
 if type_of_food == 'JAPANESE':
-  if cost == 'C':
-    print('idk')
-  elif (type_of_food == 'JAPANESE') and (cost == 'N'):
-    print('somewhere')
-  elif (type_of_food == 'JAPANESE') and (cost == 'F'):  
-    print('Masayoshi')
+    if cost == 'C':
+        print('idk')
+    elif cost == 'N':
+        print('somewhere')
+    elif cost == 'F':
+        print('Masayoshi')
