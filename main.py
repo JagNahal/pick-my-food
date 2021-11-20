153418abac18f0739a27d6542649ef631161da36
Jagjeevan Nahal <93895203+JagNahal@users.noreply.github.com>
2021-11-20 02:12:47 +0000

    reorganized code

--- main.py
+++ main.py
@@ -1,40 +1,82 @@
 import os
 import requests
 import random
-my_secret = os.environ['API Key']
+
+# TODO: Add mexican cuisine as an option
+CUISINE_CODES = {
+    'ASIAN': 10659,
+    'INDIAN': 10346,
+    'SEAFOOD': 10643,
+    'THAI': 10660,
+    'JAPANESE': 5473,
+    'EUROPEAN': 10654,
+    'AMERICAN': 9908,
+    'CHINESE': 5379,
+    'SUSHI': 10653,
+    'ITALIAN': 4617,
+    'FRENCH': 5086,
+    'FAST FOOD': 10646,
+    'KOREAN': 10661,
+    'VIETNAMESE': 10675,
+}
+
+def get_cuisines_prompt():
+    """Returns a friendly sorted list of cuisine options for display in prompt"""
+    cuisines = []
+    for cuisine in CUISINE_CODES:
+        cuisines.append(cuisine)
+    cuisines_prompt = '\n'.join(sorted(cuisines)).title()
+    return cuisines_prompt
+
+
 def which_cuisine():
-  """User inputs which cuisine they want to eat"""
-  cuisine_type = input("Which Cuisine?\n\nAmerican\nAsian\nChinese\nFrench\nFast Food\nIndian\nItalian\nJapanese\nKorean\nSeafood\nSushi\nThai\nVietnamese\n\n").upper()
-  return cuisine_type
+    """Grabs user input and returns the cuisine type chosen."""
+    cuisine_type = input(
+        f"Which Cuisine?\n\n{get_cuisines_prompt()}\n\n> ").upper()
+    return cuisine_type
 
-def get_restaurants():  
-  """Returns restaurnts from Travel Advisor from user's inputted cuisine choice"""
-  
-  querystring = {"bl_latitude":"49.204589","tr_latitude":"49.301397","bl_longitude":"-123.223932","tr_longitude":"-122.702477","min_rating":"4","restaurant_tagcategory_standalone":"10591","restaurant_tagcategory":"10591","limit":"50","currency":"CAD","combined_food":"10646","lunit":"km","lang":"en_US"}
 
-  #Add japanese and mexican cuisine types
-  restaurant_codes = {'VIETNAMESE':10675, 'ASIAN':10659,'INDIAN':10346, 'SEAFOOD':10643, 'THAI':10660, 'AMERICAN':9908, 'CHINESE':5379,'SUSHI':10653, 'ITALIAN':4617, 'FRENCH':5086, 'FAST FOOD':10646, 'KOREAN':10661,}
+def get_restaurants(cuisine_type):
+    """Returns restaurants from Travel Advisor from user's inputed cuisine choice"""
 
-  headers = {
-    'x-rapidapi-host': "travel-advisor.p.rapidapi.com",
-    'x-rapidapi-key': my_secret
+    query_string = {
+        "bl_latitude": "49.204589",
+        "tr_latitude": "49.301397",
+        "bl_longitude": "-123.223932",
+        "tr_longitude": "-122.702477",
+        "min_rating": "4",
+        "restaurant_tagcategory_standalone": "10591",
+        "restaurant_tagcategory": "10591",
+        "limit": "50",
+        "currency": "CAD",
+        "combined_food": f"{CUISINE_CODES[cuisine_type]}",
+        "lunit": "km",
+        "lang": "en_US"
     }
-  cuisine_type = which_cuisine()
-  querystring['combined_food'] = restaurant_codes[cuisine_type]
 
-  restaurants = requests.request("GET", "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary", headers=headers, params=querystring)
+    headers = {
+        'x-rapidapi-host': "travel-advisor.p.rapidapi.com",
+        'x-rapidapi-key': os.environ['API_KEY']
+    }
 
-  print(restaurants)
+    restaurants = requests.request(
+        "GET",
+        "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary",
+        headers=headers,
+        params=query_string)
 
-  restaurants = restaurants.json()['data']
+    restaurants = restaurants.json()['data']
 
-  print(restaurants)
-  return restaurants
+    return restaurants
 
 
 def choose_restaurant():
-  restaurants = get_restaurants()
-  print(random.choice(restaurants)["name"])
+    cuisine_type = which_cuisine()
+    restaurants = get_restaurants(cuisine_type)
+    chosen_restaurant = random.choice(restaurants)
+    return chosen_restaurant
 
-choose_restaurant()
-
+if __name__ == "__main__":
+  restaurant = choose_restaurant()
+  print(restaurant["name"])
+  print(restaurant["website"])
--- price_cuisine_functions.py
+++ price_cuisine_functions.py
@@ -9,12 +9,3 @@ def price():
             print('\nInvalid Input (Choose between F, N, or C)\n ')
             continue
     return cost
-
-
-def cuisine():
-    cuisine_type = input("\nWhich Cuisine? ").upper()
-    return cuisine_type
-
-def dish():
-  dish_type = input('\nSpecific Dish In Mind? ')
-  return dish_type
